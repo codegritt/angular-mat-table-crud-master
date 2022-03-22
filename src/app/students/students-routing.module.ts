6decd767084489a6d3a3de1743550af2a8bc63d7
gokul <gokulchandan18@gmail.com>
2022-03-22 13:09:37 +0530

    commit

--- src/app/students/students-routing.module.ts
+++ src/app/students/students-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StudentsListComponent } from './students-list/students-list.component';
-import { StudentsNotesComponent } from './students-notes/students-notes.component';
+
 
 const routes: Routes = [
     {
         path: '',
         children: [
-            { path: 'list', component: StudentsListComponent },
-            { path: 'notes', component: StudentsNotesComponent }
+            { path: 'list', component: StudentsListComponent }
         ]
     }
 ];
